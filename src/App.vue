ea90a8c233a3f8dbc0e0373225d5b255b81fd1ed
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2021-02-22 17:32:58 -0300

    Pequenos exemplos apos a explicaçao

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Exemplo></Exemplo>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import Exemplo from './components/Exemplo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Exemplo
   }
 }
 </script>
---
+++ src/components/Exemplo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{ mensagem }}
+
+    <input type="text" v-model="mensagem" />
+  </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {
+            mensagem: 'Olá, mundo!'
+        }
+    }
+};
+</script>
+
+<style scoped>
+div {
+    border: 1px solid black;
+}
+</style>
